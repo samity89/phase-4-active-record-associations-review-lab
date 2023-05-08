75d42f7aef64ebc3df6b2f2c5017661055b58db4
Samuel Amity <sam.amity@gmail.com>
2023-05-08 11:30:05 -0400

    lab completed

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,8 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.integer :ride_id
+      t.integer :passenger_id
       t.timestamps
     end
   end
--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,7 +1,8 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
-
+      t.integer :taxi_id
+      t.integer :ride_id
       t.timestamps
     end
   end
